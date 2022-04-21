85540d48eb1e19d2a989e73dd26af72c290e04bc
simonp20 <parker_simon@brown.edu>
2022-04-21 13:59:20 -0400

    backend can connect to database

--- backend/src/main/java/com/brown/app/App.java
+++ backend/src/main/java/com/brown/app/App.java
@@ -1,5 +1,12 @@
 package com.brown.app;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -10,5 +17,9 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("This is a test to see if the running changes");
+        String connectionString = System.getProperty("mongodb.uri");
+        try (MongoClient mongoClient = MongoClients.create(connectionString)) {
+            System.out.println(mongoClient.listDatabaseNames().into(new ArrayList<>()));
+        }
     }
 }
