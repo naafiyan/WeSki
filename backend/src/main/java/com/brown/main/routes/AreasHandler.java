1314eb765289f211cc9ebd386a22a3e783e8072d
Naafiyan Ahmed <naafiyan@gmail.com>
2022-04-28 23:02:38 -0400

    feat: added basic get route functionality

--- backend/src/main/java/com/brown/main/routes/AreasHandler.java
+++ backend/src/main/java/com/brown/main/routes/AreasHandler.java
@@ -1,16 +1,9 @@
 package com.brown.main.routes;
 
-import com.brown.main.database.Database;
-import com.brown.main.database.MongoHelper;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class AreasHandler {
 
