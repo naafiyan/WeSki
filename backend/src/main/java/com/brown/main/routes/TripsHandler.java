cb7704b4db62a1d834fb426551cd56c17c90abf8
simonp20 <parker_simon@brown.edu>
2022-05-04 16:43:56 -0400

    automatically generated commit message

--- backend/src/main/java/com/brown/main/routes/TripsHandler.java
+++ backend/src/main/java/com/brown/main/routes/TripsHandler.java
@@ -1,4 +1,55 @@
 package com.brown.main.routes;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.mongodb.client.model.Filters.eq;
+
 public class TripsHandler {
+
+  public static JsonObject getAllTrips(MongoDatabase db) {
+    ArrayList<Document> tripsDocs = db.getCollection("trips").find().into(new ArrayList<Document>());
+    // convert areasDocs to json
+    List<String> tripStrings = new ArrayList<>();
+    for (Document areaDoc : tripsDocs) {
+      tripStrings.add(areaDoc.toJson());
+    }
+
+    ArrayList<JsonObject> trips = new ArrayList<JsonObject>();
+
+    Gson gson = new Gson();
+    String jsonString = null;
+    for (Document doc : tripsDocs) {
+      jsonString = gson.toJson(doc);
+      trips.add(new Gson().fromJson(jsonString, JsonObject.class));
+    }
+    // return all the areas as a json
+    JsonObject res = new JsonObject();
+    res.add("trips", gson.toJsonTree(trips));
+    return res;
+  }
+
+  public static String getTrip(MongoDatabase db, String id) {
+    ArrayList<Document> tripsDocs = db.getCollection("trips").find(eq("_id", id)).into(new ArrayList<Document>());
+    return tripsDocs.get(0).toJson();
+  }
+
+  public static String getComments(MongoDatabase db, String id) {
+    ArrayList<Document> commentsDocs = db.getCollection("comments").find(eq("_id", id)).into(new ArrayList<Document>());
+    Gson gson = new Gson();
+    ArrayList<String> comments = new ArrayList<>();
+    for (Document doc: commentsDocs) {
+      comments.add(gson.toJson(doc));
+    }
+    Map<String, ArrayList<String>> res = new HashMap<>();
+    res.put("comments", comments);
+    return gson.toJson(res);
+  }
 }
