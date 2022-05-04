506294c05e9c32c8067e4e4427fe3318122a0b4f
Brandon Diaz <77746846+brandondia-z@users.noreply.github.com>
2022-05-04 15:06:58 -0400

    search bar almost done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MountainPage from "./pages/MountainPage";
 import Homepage from "./pages/Homepage";
-import TestPage from "./pages/Test";
 import { createTheme, ThemeProvider } from "@mui/material";
 import Header from "./components/Header";
 import MyAccountPage from "./pages/MyAccountPage"
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,20 +1,70 @@
-import Button from "@mui/material/Button";
-import { Box, FormControlLabel, FormGroup, Slider, Stack, Switch, TextField } from "@mui/material";
-import Header from "../components/Header";
-import Form from "react-bootstrap/Form"
-import MySlider from "../components/Slider"
-import Input from "@mui/material/Input"
-import {Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import StyledSlider from "../components/Slider";
+import * as React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import LocationSearchingOutlinedIcon from '@mui/icons-material/LocationSearchingOutlined';
 
 function Homepage() {
-    return(<TextField
-    name="startDate"
-    label="Start Date"
-    InputLabelProps={{ shrink: true, required: true, style: { fontWeight: 700, color:'#1E1E1E', fontFamily:"Roboto", fontSize:'18px'} }}
-    type="date"
-    InputProps={{ style: {border: "1px solid #C2C2C2", padding: 6, width:480, height:51} }}
-    />
+
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: '#5A9B85'
+            },
+            secondary: {
+              main: '#5A9B85'
+            }
+          }
+      });
+
+    const [startValue, setStartValue] = React.useState<Date | null>(
+        new Date('2022-05-04T21:11:54'),
+      );
+
+      const [endValue, setEndValue] = React.useState<Date | null>(
+        new Date('2022-05-04T21:11:54'),
+      );
+    
+      const handleStartChange = (newValue: Date | null) => {
+        setStartValue(newValue);
+      };
+
+      const handleEndChange = (newValue: Date | null) => {
+        setEndValue(newValue);
+      };
+
+    return(
+        <div className="rectangle">
+            <p className="trip-text">Plan Your Trip</p>
+            <div className="date-bar">
+                <ThemeProvider theme={theme}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DesktopDatePicker
+                        label="Start Date" 
+                        inputFormat="MM/dd/yyyy" 
+                        value={startValue} 
+                        onChange={handleStartChange} 
+                        renderInput={(params) => <TextField {...params} sx={{width: 175, position: "absolute", top: 10, left: 20}}/>}
+                        />
+                        <DesktopDatePicker
+                        label="End Date" 
+                        inputFormat="MM/dd/yyyy" 
+                        value={endValue} 
+                        onChange={handleEndChange} 
+                        renderInput={(params) => <TextField {...params} sx={{width: 175, position: "absolute", top: 10, right: 20}}/>}
+                        />
+                    </LocalizationProvider>
+                </ThemeProvider>
+                <div className="division-bar"></div>
+            </div>
+            <div className="search-button">
+                <LocationSearchingOutlinedIcon className="search-icon"/>
+            </div>
+        </div>
+        
+
     );
    
 }
