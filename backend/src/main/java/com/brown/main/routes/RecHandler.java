b237ffdc006ab53fad0ccfb8fcffc02df4375dde
Colin Savage <colin_f_savage@brown.edu>
2022-05-10 22:33:41 -0400

    yuh

--- backend/src/main/java/com/brown/main/routes/RecHandler.java
+++ backend/src/main/java/com/brown/main/routes/RecHandler.java
@@ -1,16 +1,21 @@
 package com.brown.main.routes;
 
+import com.brown.main.database.Database;
+import com.brown.main.database.MongoHelper;
 import com.brown.main.recsys.Areas;
 import com.brown.main.recsys.Recommendation;
 import com.google.common.collect.ImmutableMap;
+import com.mongodb.client.model.Filters;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.brown.main.recsys.TreeInfo;
 import com.google.gson.Gson;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import spark.Request;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -30,6 +35,10 @@ public class RecHandler {
     info.setSkillLevel(Double.parseDouble(reqMap.get("difficultyPref")));
     info.setDistance(1-Double.parseDouble(reqMap.get("locPref")));
     List<String> nearest = new Recommendation(info).getNearest();
+    List<String> best = new ArrayList<>();
+    for (String s: nearest){
+      best.add(new MongoHelper("notSkiQL").getDb("notSkiQL").getCollection("areas").find(Filters.eq("name", s)).first().toJson());
+    }
     return nearest;
   }
 }
