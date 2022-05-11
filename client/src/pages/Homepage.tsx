4c70b49d8a03fdc153713fbfb8893209b17ce546
sarah roberts <sarah_roberts2@brown.edu>
2022-05-10 21:50:00 -0400

    silly silly

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -84,7 +84,7 @@ export default function Homepage() {
                 </Nav.Link>
                 <br/>
 
-                <Carousel>
+                <Carousel fade>
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
@@ -92,68 +92,118 @@ export default function Homepage() {
                             alt="First slide"
                         />
                         <Carousel.Caption>
-                            <h3>Killington</h3>
-                            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            <h3 style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 0,
+                            }}>Killington </h3>
+                            <p style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 10,
+                            }}>The Northeast’s largest ski venue, located in central VT.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
-                            src={require('../images/download.jpg')}
+                            src={require('../images/stratton.jpeg')}
                             alt="Second slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Stratton</h3>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            <h3 style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 0,
+                            }}>Stratton</h3>
+                            <p style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 10,
+                            }}>Located in southern VT, Stratton is a great ski area for beginners and advanced skiers.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
-                            src={require('../images/download.jpg')}
+                            src={require('../images/attitash.jpeg')}
                             alt="Third slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Attitash</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <h3 style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 0,
+                            }}>Attitash</h3>
+                            <p style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 10,
+                            }}>It has an infamously slow chairlift to the top, but two peaks!</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
-                            src={require('../images/download.jpg')}
+                            src={require('../images/sundayriver2.jpg')}
                             alt="Third slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Sunday River</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <h3 style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 0,
+                            }}>Sunday River</h3>
+                            <p style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 10,
+                            }}>Come ski at your Happy Place in Maine. Bring the White Heat!</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             className="d-block w-100"
-                            src={require('../images/download.jpg')}
+                            src={require('../images/skibutternut.jpeg')}
                             alt="Third slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Ski Butternut</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <h3 style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 0,
+                            }}>Ski Butternut</h3>
+                            <p style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 10,
+                            }}>A great family ski area located on Warner Mountain in western MA.</p>
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
                         <img
                             className="d-block w-100 "
-                            src={require('../images/download.jpg')}
+                            src={require('../images/skisundown.jpeg')}
                             alt="Third slide"
                         />
 
                         <Carousel.Caption>
-                            <h3>Ski Sundown</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <div>
+                            <h3 style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 0,
+                            }}>Ski Sundown</h3>
+                            <p style={{
+                                backgroundColor: "#5A9B85",
+                                opacity: "0.8",
+                                marginBottom: 10,
+                            }}>A great mountain for beginners located 30 minutes from Hartford, CT.</p>
+                            </div>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
