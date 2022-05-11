d6c01d2b6be9feb0fc7d266a10de3793ed5e55c4
simonp20 <parker_simon@brown.edu>
2022-05-10 20:34:26 -0400

    added table route

--- backend/src/main/java/com/brown/main/routes/TableHandler.java
+++ backend/src/main/java/com/brown/main/routes/TableHandler.java
@@ -7,31 +7,32 @@ import java.util.Map;
 import com.brown.main.models.Area;
 import com.google.gson.Gson;
 import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 public class TableHandler {
   public static String getTable(MongoDatabase db) {
-    ArrayList<Area> areaList = db.getCollection("areas", Area.class).find().into(new ArrayList<>());
+    ArrayList<Document> areaList = db.getCollection("areas").find().into(new ArrayList<>());
     ArrayList<ArrayList<String>> headersList = new ArrayList<>();
     ArrayList<String> headers = new ArrayList<>();
     headersList.add(headers);
     headersList.get(0).add("Ski Area");
-    headersList.get(0).add("Weather");
     headersList.get(0).add("# Trails Open");
     headersList.get(0).add("Acreage Open");
-    headersList.get(0).add("Base");
-    headersList.get(0).add("Recent Snowfall");
-    headersList.get(0).add("Weekend Price");
+    headersList.get(0).add("Price");
+    headersList.get(0).add("Temperature");
+    headersList.get(0).add("Difficulty");
 
     ArrayList<ArrayList<String>> rows = new ArrayList<ArrayList<String>>();
-    for(Area area: areaList) {
+    System.out.println(areaList.get(0));
+    for(Document area: areaList) {
       ArrayList<String> row = new ArrayList<>();
-      row.add(area.getName());
-      row.add("placeholder");
-      row.add(Integer.toString(area.getTrails().size()));
-      row.add(Double.toString(area.getAcreage()));
-      row.add(Double.toString(area.getBase()));
-      row.add(Double.toString(area.getRecent_snowfall()));
-      row.add("placeholder");
+      row.add(area.get("name").toString());
+      row.add(area.get("num_trails").toString());
+      row.add(area.get("acreage").toString());
+      row.add(area.get("price").toString());
+      row.add(area.get("temperature").toString());
+      row.add(area.get("difficulty").toString());
+      rows.add(row);
     }
 
     Map<String, ArrayList<ArrayList<String>>> res = new HashMap<>();
