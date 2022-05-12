14d421e58d57d20070f761874474e19acee2dfc5
Naafiyan Ahmed <naafiyan@gmail.com>
2022-05-12 18:44:29 -0400

    style: added comments

--- client/src/components/FavoriteMountain.tsx
+++ client/src/components/FavoriteMountain.tsx
@@ -1,41 +1,53 @@
-import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 import { useState } from 'react';
 
+/**
+ * Type definition for the props of the component.
+ */
 type FavMountainProps = {
   favMountain: string;
   setFavMountain: (value: string) => void;
 }
+
+/**
+ * The React functional component for the Favorite Mountain component.
+ * @param props The props of the component.
+ * @returns The rendered component.
+ */
 export default function FavoriteMountain(props: FavMountainProps) {
 
+  // state variables for the areas
   const [areas, setAreas] = useState<resort[]>([]);
 
+  /**
+   * Type definition for the props of the component.
+   */
   type resort = {
     label: string
     ID: number
-  }  
+  }
 
   async function setSkiAreas() {
     const res = await fetch("http://localhost:4567/areas", {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
     })
     const resJson = await res.json();
     const allAreas = resJson.areas;
     let avaliableSkiMountains: resort[] = []
-    for(let i = 0; i < allAreas.length; i++) {
+    for (let i = 0; i < allAreas.length; i++) {
       let nextName = allAreas[i].name;
       let area: resort = {
         label: nextName,
-        ID: i        
+        ID: i
       }
       avaliableSkiMountains.push(area);
     }
     setAreas(avaliableSkiMountains);
-    
+
   }
 
   setSkiAreas()
@@ -49,7 +61,7 @@ export default function FavoriteMountain(props: FavMountainProps) {
       onInputChange={(event, newInputValue) => {
         props.setFavMountain(newInputValue);
       }}
-      renderInput={(params) => <TextField {...params} label={props.favMountain}/>}
+      renderInput={(params) => <TextField {...params} label={props.favMountain} />}
     />
   );
 }
