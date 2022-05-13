a5e0b38df42424ae5d4032c8a2e526003b6bb170
Brandon Diaz <77746846+brandondia-z@users.noreply.github.com>
2022-05-13 15:04:13 -0400

    socket

--- client/src/pages/CompareVenue.tsx
+++ client/src/pages/CompareVenue.tsx
@@ -11,8 +11,10 @@ import Paper from '@mui/material/Paper';
 
 export default function CompareVenue() {
 
+    //functions used to set the row and header values from the data
     const [rows, setRows] = useState<string[][]>([[]]);
     const [headers, setHeaders] = useState<string[]>([]);
+    const [sortCol, setSortCol] = useState<string>('');
 
     // fetch the data from api endpoint
     const fetchTables = async () => {
@@ -22,6 +24,7 @@ export default function CompareVenue() {
         console.log(data);
         setHeaders(data.headers);
         setRows(data.rows);
+        console.log("got the data")
     }
 
     // useEffect with tables dependency to map and set the table names
@@ -34,6 +37,7 @@ export default function CompareVenue() {
         fetchTables();
     }
 
+    // table styling
     const StyledTableCell = styled(TableCell)(({ theme }) => ({
         [`&.${tableCellClasses.head}`]: {
           backgroundColor: theme.palette.common.black,
@@ -44,6 +48,7 @@ export default function CompareVenue() {
         },
     }));
     
+    // styling per each new row w/ special row for headers
     const StyledTableRow = styled(TableRow)(({ theme }) => ({
     '&:nth-of-type(odd)': {
         backgroundColor: theme.palette.action.hover,
@@ -54,45 +59,72 @@ export default function CompareVenue() {
     },
     }));
 
-    // function isNumeric(value: string) {
-    //     return /^-?\d+$/.test(value);
-    // }
+    // function used in the sortTable function
+    function isNumeric(value: string) {
+        return /^-?\d+$/.test(value);
+    }
 
-    // const sortTable = () => {
-    //     for(var i=0; i<rows.length; i+=1) {
-    //         for(var j=i; j<rows.length; j+=1) {
-    //             if(isNumeric(rows[j][props.sortCol])){
-    //                 if (parseFloat(rows[j][props.sortCol]) < parseFloat(rows[i][props.sortCol])) {
-    //                     var temp = rows[j];
-    //                     rows[j] = rows[i];
-    //                     rows[i] = temp;
-    //                 };
-    //             } else {
-    //                 if (rows[j][props.sortCol] < rows[i][props.sortCol]) {
-    //                     var temp = rows[j];
-    //                     rows[j] = rows[i];
-    //                     rows[i] = temp;
-    //                 };
-    //             }
-    //         };
-    //     };
-    //     const tab: table = {
-    //         headerList: props.tableInfo.headerList,
-    //         rowMap: rows,
-    //     }
-    //     props.setRowMap(tab);
-    // };
+    const sortTable = () => {
+        console.log("tryna sort")
+        var ro: number = 0
+        switch(sortCol) {
+            case "Ski Area": default:
+                ro = 0;
+                break;
+            case "# Trails Open":
+                ro = 1;
+                break;
+            case "Acreage Open":
+                ro = 2;
+                break;
+            case "Price":
+                ro = 3;
+                break;
+            case "Temperature":
+                ro = 4;
+                break;
+            case "Difficulty":
+                ro = 5;
+                break;
+        }
+        for(var i=0; i<rows.length; i+=1) {
+            for(var j=i; j<rows.length; j+=1) {
+                if(isNumeric(rows[j][ro])){
+                    if (parseFloat(rows[j][ro]) < parseFloat(rows[i][ro])) {
+                        var temp = rows[j];
+                        rows[j] = rows[i];
+                        rows[i] = temp;
+                    };
+                } else {
+                    if (rows[j][ro] < rows[i][ro]) {
+                        var temp = rows[j];
+                        rows[j] = rows[i];
+                        rows[i] = temp;
+                    };
+                }
+            };
+        };
+    };
 
-    // useEffect(sortTable, [props.sortCol]);
+    // useEffect(sortTable, [sortCol]);
 
     return (
         <div>
+            {/* Page Title */}
             <h1>Compare Venues</h1>
             <div id="body">
-                <TableContainer component={Paper}>
+                <select name='colselect' id='col-select' onChange={e => setSortCol(e.target.value)}>
+                    <option>Ski Area</option>
+                    <option># Trails Open</option>
+                    <option>Acreage Open</option>
+                    <option>Price</option>
+                    <option>Temperature</option>
+                    <option>Difficulty</option>
+                </select>
+                <TableContainer component={Paper}> {/* Styling component for table */}
                     <Table aria-label="table with ski venue data">
                         <TableHead>
-                            <StyledTableRow>
+                            <StyledTableRow> {/* Styled Table Row for Headers */}
                                 <StyledTableCell>Ski Area</StyledTableCell>
                                 <StyledTableCell># Trails Open</StyledTableCell>
                                 <StyledTableCell>Acreage Open</StyledTableCell>
@@ -102,6 +134,7 @@ export default function CompareVenue() {
                             </StyledTableRow>
                         </TableHead>
                         <TableBody>
+                            {/* map function loops through all data elements out in rows sequentially */}
                             {rows.map(item => <StyledTableRow>
                                 {item.map(data => <StyledTableCell>{data}</StyledTableCell>)}
                             </StyledTableRow>)}
